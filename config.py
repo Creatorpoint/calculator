eb9ab69954d662fe298215c503fbd597ce73c3f6
Creatorpoint <pk4030794@gmail.com>
2026-05-27 11:39:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+FORCE_GROUP = os.getenv("FORCE_GROUP")
